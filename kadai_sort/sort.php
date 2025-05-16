881c6ac2fe0ee87c585f5470196b5e08c5dddb04
mayuko-hirahara <fuji.maylor3@gmail.com>
2025-05-16 17:44:28 +0900

    kadai_sort提出

---
+++ kadai_sort/sort.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+    <p>昇順にソートします。<br>
+        <?php
+        $nums = [15, 4, 18, 23, 10 ];
+        sort($nums);
+        foreach ($nums as $num) {
+          echo $num . '<br>';
+        }
+        ?>
+    降順にソートします。<br>
+        <?php
+        $nums = [15, 4, 18, 23, 10 ];
+        rsort($nums);
+        foreach ($nums as $num) {
+          echo $num . '<br>';
+        }
+        ?>
+    </p>
+</body>
+
+</html>
